380e8fdd974efbaae9bd6430f0e9763940be02a3
Roman Eskin <r.eskin@arenadata.io>
2024-01-11 21:22:22 +1000

    Simplify and refactor pg_stat_statements test

--- contrib/pg_stat_statements/sql/test_pg_stat_statements.sql
+++ contrib/pg_stat_statements/sql/test_pg_stat_statements.sql
@@ -3,17 +3,9 @@
 \! gpstop -raq -M fast;
 \c
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-DROP TABLE IF EXISTS table_test_pg_stat_statements;
+DROP TABLE IF EXISTS t;
 -- end_ignore
--- simple test to check there is no warnings during jumbling of the query
-CREATE TABLE table_test_pg_stat_statements
-(
-    item1 integer,
-    item2 integer,
-    item3 integer,
-    item4 integer
-)
-DISTRIBUTED BY (item1);
+CREATE TABLE t(a int, b int) DISTRIBUTED BY (a);
 
 -- Known issue: query is not added to pg_stat_statements statistics in
 -- case it is planned by GPORCA. So disable GPORCA during tests.
@@ -21,69 +13,19 @@ SET optimizer='off';
 
 SELECT pg_stat_statements_reset();
 
--- launch 2 equivalent queries  
-SELECT
-    GROUPING(item1)
-FROM
-    table_test_pg_stat_statements
-WHERE
-    item3 = 0
-GROUP BY ROLLUP(item1, item2);
-
-SELECT
-    GROUPING(item1)
-FROM
-    table_test_pg_stat_statements
-WHERE
-    item3 = 1
-GROUP BY ROLLUP(item1, item2);
-
--- pg_stat_statements statistics should have 2 calls for 1 entry
--- corresponding to the two preceding SELECT queries
-SELECT query, calls FROM pg_stat_statements ORDER BY QUERY;
-
+SELECT GROUPING (a) FROM t GROUP BY ROLLUP(a, b);
 -- launch not equivalent query
-SELECT
-    GROUPING(item2)
-FROM
-    table_test_pg_stat_statements
-WHERE
-    item3 = 0
-GROUP BY ROLLUP(item1, item2);
+SELECT GROUPING (b) FROM t GROUP BY ROLLUP(a, b);
 
--- check that it has separate entry
+-- check that 2 queries have separate entries
 SELECT query, calls FROM pg_stat_statements ORDER BY QUERY;
 
 -- check that different grouping options result in separate entries
-SELECT
-    COUNT (*)
-FROM
-    table_test_pg_stat_statements
-GROUP BY ROLLUP(item1, item2, item3, item4);
-
-SELECT
-    COUNT (*)
-FROM
-    table_test_pg_stat_statements
-GROUP BY CUBE(item1, item2, item3, item4);
-
-SELECT
-    COUNT (*)
-FROM
-    table_test_pg_stat_statements
-GROUP BY GROUPING SETS(item1, item2, item3, item4);
-
-SELECT
-    COUNT (*)
-FROM
-    table_test_pg_stat_statements
-GROUP BY GROUPING SETS((item1, item2), (item3, item4));
-
-SELECT
-    COUNT (*)
-FROM
-    table_test_pg_stat_statements
-GROUP BY item1, item2, item3, item4;
+SELECT COUNT (*) FROM t GROUP BY ROLLUP(a, b);
+SELECT COUNT (*) FROM t GROUP BY CUBE(a, b);
+SELECT COUNT (*) FROM t GROUP BY GROUPING SETS(a, b);
+SELECT COUNT (*) FROM t GROUP BY GROUPING SETS((a), (a, b));
+SELECT COUNT (*) FROM t GROUP BY a, b;
 
 SELECT query, calls FROM pg_stat_statements ORDER BY QUERY;
 
@@ -91,23 +33,14 @@ SELECT query, calls FROM pg_stat_statements ORDER BY QUERY;
 -- all should result in separate entries
 SELECT pg_stat_statements_reset();
 
-SELECT
-    COUNT (*)
-FROM
-    table_test_pg_stat_statements
-GROUP BY ROLLUP(item1, item2);
-
-SELECT
-    COUNT (*)
-FROM
-    table_test_pg_stat_statements
-GROUP BY ROLLUP(item2, item3);
+SELECT COUNT (*) FROM t GROUP BY ROLLUP(a, b);
+SELECT COUNT (*) FROM t GROUP BY ROLLUP(b);
 
 SELECT query, calls FROM pg_stat_statements ORDER BY QUERY;
 
 RESET optimizer;
 
-DROP TABLE table_test_pg_stat_statements;
+DROP TABLE t;
 -- start_ignore
 \! gpconfig -r shared_preload_libraries;
 \! gpstop -raq -M fast;
